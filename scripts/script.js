4c3bf7ad295527d3bcfbe95f9db8ba27dfcffac3
HenriIncerti <henrique_incerti@estudante.sesisenai.org.br>
2026-04-13 11:11:20 -0300

    add - Validações finais

--- scripts/script.js
+++ scripts/script.js
@@ -1,32 +1,108 @@
+let listaDoacoes = [];
+
 document.getElementById("doacaoSangue").addEventListener("submit", function (e) {
 
+    e.preventDefault(); 
+
+    let falha = false;
+
     let nome = document.getElementById("nome").value;
-    if(nome === "") return alert("Insira seu nome!");
-    if(nome.length < 10) return alert("Insira o seu nome completo!");
-    
+    if (nome === "") {
+        alert("Insira seu nome!");
+        falha = true;
+        return false;
+    }
+    if (nome.length < 10) {
+        alert("Insira o seu nome completo!");
+        falha = true;
+        return false;
+    }
+
     let email = document.getElementById("email").value;
-    if(email === "") return alert("Insira um E-mail!");
-    if(!email.includes("@")) return alert("Insira um E-mail válido!");
+    if (email === "") {
+        alert("Insira um E-mail!");
+        falha = true;
+        return false;
+    }
+    if (!email.includes("@")) {
+        alert("Insira um E-mail válido!");
+        falha = true;
+        return false;
+    }
 
     let idade = document.getElementById("idade").value;
-    if(idade === "") return alert("Insira sua idade!");
-    if(idade < 16) return alert("Você não tem idade mínima para a doação de sangue!");
+    if (idade === "") {
+        alert("Insira sua idade!")
+        falha = true;
+        return false;
+    }
+    if (idade < 16) {
+        alert("Você não tem idade mínima para a doação de sangue!");
+        falha = true;
+        return false;
+    }
 
     let peso = document.getElementById("peso").value;
-    if(peso === "") return alert("Insira seu peso!");
-    if(peso < 50) return alert("Você não tem peso suficiente para doar sangue!");
+    if (peso === "") {
+        alert("Insira seu peso!");
+        falha = true;
+        return false;
+    }
+    if (peso < 50) {
+        alert("Você não tem peso suficiente para doar sangue!");
+        falha = true;
+        return false;
+    }
 
     let tipoSanguineo = document.getElementById("tipoSanguineo").value;
-    if(tipoSanguineo === "") return alert("Insira seu tipo sanguíneo!");
+    if (tipoSanguineo === "") {
+        alert("Insira seu tipo sanguíneo!");
+        falha = true;
+        return false;
+    }
 
-    let tel = parseFloat(document.getElementById("tel")).value;
-    if(tel === "") return alert("Insira seu telefone!");
-    if(isNaN(tel)) return alert("Insira um telefone válido!");
+    let tel = parseFloat(document.getElementById("tel").value);
+    if (isNaN(tel)) {
+        alert("Insira um telefone válido!");
+        falha = true;
+        return false;
+    }
 
     let cidade = document.getElementById("cidade").value;
-    if(cidade === "") return alert("Insira sua cidade!");
+    if (cidade === "") {
+        alert("Insira sua cidade!");
+        falha = true;
+        return false;
+    }
 
     let estado = document.getElementById("estado").value;
-    if(estado === "") return alert("Insira seu estado!");
+    if (estado === "") {
+        alert("Insira seu estado!");
+        falha = true;
+        return false;
+    }
 
+    if (falha) {
+        e.preventDefault();
+        return;
+    }
+
+    let doacao = {
+        nome: nome,
+        email: email,
+        idade: idade,
+        peso: peso,
+        tipoSanguineo: tipoSanguineo,
+        telefone: tel,
+        cidade: cidade,
+        estado: estado
+    };
+
+    console.log(doacao);
+
+    listaDoacoes.push(doacao);
+
+    console.log(listaDoacoes);
+
+    alert("Cadastro realizado com sucesso!");
 });
